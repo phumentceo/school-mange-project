6229a5e4df92fa6f3fd6f97631065556db54f867
phumentceo <roungchanrith8@gmail.com>
2024-12-18 13:54:08 +0700

    Profile Design

--- resources/views/principal/profile/profile.blade.php
+++ resources/views/principal/profile/profile.blade.php
@@ -29,60 +29,60 @@
             <ul class="nav nav-tabs nav-tabs-bordered">
 
               <li class="nav-item">
-                <button class="nav-link active" data-bs-toggle="tab" data-bs-target="#profile-overview">Overview</button>
+                <button class="nav-link active" data-bs-toggle="tab" data-bs-target="#profile-overview">ព័ត្នមានផ្ទាល់ខ្លួន</button>
               </li>
 
               <li class="nav-item">
-                <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-edit">Edit Profile</button>
+                <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-edit">កែសម្រួល Profile</button>
               </li>
 
               
 
               <li class="nav-item">
-                <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-change-password">Change Password</button>
+                <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-change-password">ផ្លាស់ប្តូរ Password</button>
               </li>
 
             </ul>
             <div class="tab-content pt-2">
 
-              <div class="tab-pane fade show active profile-overview" id="profile-overview">
-                <h5 class="card-title">About</h5>
+              <div class="tab-pane fade show active profile-overview p-3" id="profile-overview">
+                <h5>ផ្សេងៗអំពីអ្នក</h5>
                 <p class="small fst-italic">Sunt est soluta temporibus accusantium neque nam maiores cumque temporibus. Tempora libero non est unde veniam est qui dolor. Ut sunt iure rerum quae quisquam autem eveniet perspiciatis odit. Fuga sequi sed ea saepe at unde.</p>
 
-                <h5 class="card-title">Profile Details</h5>
+               
 
                 <div class="row">
-                  <div class="col-lg-3 col-md-4 label ">Full Name</div>
+                  <div class="col-lg-3 col-md-4 label ">នាម + គោត្តមាន</div>
                   <div class="col-lg-9 col-md-8">Kevin Anderson</div>
                 </div>
 
                 <div class="row">
-                  <div class="col-lg-3 col-md-4 label">Company</div>
-                  <div class="col-lg-9 col-md-8">Lueilwitz, Wisoky and Leuschke</div>
+                  <div class="col-lg-3 col-md-4 label">តួនាទី</div>
+                  <div class="col-lg-9 col-md-8">នាយកធំ</div>
                 </div>
 
                 <div class="row">
-                  <div class="col-lg-3 col-md-4 label">Job</div>
-                  <div class="col-lg-9 col-md-8">Web Designer</div>
+                  <div class="col-lg-3 col-md-4 label">របប</div>
+                  <div class="col-lg-9 col-md-8">នាយកសាលា</div>
                 </div>
 
                 <div class="row">
-                  <div class="col-lg-3 col-md-4 label">Country</div>
-                  <div class="col-lg-9 col-md-8">USA</div>
+                  <div class="col-lg-3 col-md-4 label">ប្រទេសកំណើត</div>
+                  <div class="col-lg-9 col-md-8">ព្រះរាជាណាចក្រកម្ពុជា</div>
                 </div>
 
                 <div class="row">
-                  <div class="col-lg-3 col-md-4 label">Address</div>
+                  <div class="col-lg-3 col-md-4 label">អាសយដ្ឋាន</div>
                   <div class="col-lg-9 col-md-8">A108 Adam Street, New York, NY 535022</div>
                 </div>
 
                 <div class="row">
-                  <div class="col-lg-3 col-md-4 label">Phone</div>
+                  <div class="col-lg-3 col-md-4 label">លេខទូរស័ព្ទ</div>
                   <div class="col-lg-9 col-md-8">(436) 486-3538 x29071</div>
                 </div>
 
                 <div class="row">
-                  <div class="col-lg-3 col-md-4 label">Email</div>
+                  <div class="col-lg-3 col-md-4 label">អ៊ីម៉ែល</div>
                   <div class="col-lg-9 col-md-8">k.anderson@example.com</div>
                 </div>
 
@@ -93,7 +93,7 @@
                 <!-- Profile Edit Form -->
                 <form>
                   <div class="row mb-3">
-                    <label for="profileImage" class="col-md-4 col-lg-3 col-form-label">Profile Image</label>
+                    <label for="profileImage" class="col-md-4 col-lg-3 col-form-label">រូបភាព</label>
                     <div class="col-md-8 col-lg-9">
                       <img src="assets/img/profile-img.jpg" alt="Profile">
                       <div class="pt-2">
@@ -104,91 +104,65 @@
                   </div>
 
                   <div class="row mb-3">
-                    <label for="fullName" class="col-md-4 col-lg-3 col-form-label">Full Name</label>
+                    <label for="fullName" class="col-md-4 col-lg-3 col-form-label">នាម + គោត្តនាម</label>
                     <div class="col-md-8 col-lg-9">
                       <input name="fullName" type="text" class="form-control" id="fullName" value="Kevin Anderson">
                     </div>
                   </div>
 
                   <div class="row mb-3">
-                    <label for="about" class="col-md-4 col-lg-3 col-form-label">About</label>
+                    <label for="about" class="col-md-4 col-lg-3 col-form-label">ផ្សេងៗ</label>
                     <div class="col-md-8 col-lg-9">
                       <textarea name="about" class="form-control" id="about" style="height: 100px">Sunt est soluta temporibus accusantium neque nam maiores cumque temporibus. Tempora libero non est unde veniam est qui dolor. Ut sunt iure rerum quae quisquam autem eveniet perspiciatis odit. Fuga sequi sed ea saepe at unde.</textarea>
                     </div>
                   </div>
 
                   <div class="row mb-3">
-                    <label for="company" class="col-md-4 col-lg-3 col-form-label">Company</label>
+                    <label for="company" class="col-md-4 col-lg-3 col-form-label">តួនាទី</label>
                     <div class="col-md-8 col-lg-9">
                       <input name="company" type="text" class="form-control" id="company" value="Lueilwitz, Wisoky and Leuschke">
                     </div>
                   </div>
 
                   <div class="row mb-3">
-                    <label for="Job" class="col-md-4 col-lg-3 col-form-label">Job</label>
+                    <label for="Job" class="col-md-4 col-lg-3 col-form-label">មុខរបប</label>
                     <div class="col-md-8 col-lg-9">
                       <input name="job" type="text" class="form-control" id="Job" value="Web Designer">
                     </div>
                   </div>
 
                   <div class="row mb-3">
-                    <label for="Country" class="col-md-4 col-lg-3 col-form-label">Country</label>
+                    <label for="Country" class="col-md-4 col-lg-3 col-form-label">ប្រទេស</label>
                     <div class="col-md-8 col-lg-9">
                       <input name="country" type="text" class="form-control" id="Country" value="USA">
                     </div>
                   </div>
 
                   <div class="row mb-3">
-                    <label for="Address" class="col-md-4 col-lg-3 col-form-label">Address</label>
+                    <label for="Address" class="col-md-4 col-lg-3 col-form-label">អាយសដ្ឋាន</label>
                     <div class="col-md-8 col-lg-9">
                       <input name="address" type="text" class="form-control" id="Address" value="A108 Adam Street, New York, NY 535022">
                     </div>
                   </div>
 
                   <div class="row mb-3">
-                    <label for="Phone" class="col-md-4 col-lg-3 col-form-label">Phone</label>
+                    <label for="Phone" class="col-md-4 col-lg-3 col-form-label">លេខទូរស័ព្ទ</label>
                     <div class="col-md-8 col-lg-9">
                       <input name="phone" type="text" class="form-control" id="Phone" value="(436) 486-3538 x29071">
                     </div>
                   </div>
 
                   <div class="row mb-3">
-                    <label for="Email" class="col-md-4 col-lg-3 col-form-label">Email</label>
+                    <label for="Email" class="col-md-4 col-lg-3 col-form-label">អ៊ីម៉ែល</label>
                     <div class="col-md-8 col-lg-9">
                       <input name="email" type="email" class="form-control" id="Email" value="k.anderson@example.com">
                     </div>
                   </div>
 
-                  <div class="row mb-3">
-                    <label for="Twitter" class="col-md-4 col-lg-3 col-form-label">Twitter Profile</label>
-                    <div class="col-md-8 col-lg-9">
-                      <input name="twitter" type="text" class="form-control" id="Twitter" value="https://twitter.com/#">
-                    </div>
-                  </div>
-
-                  <div class="row mb-3">
-                    <label for="Facebook" class="col-md-4 col-lg-3 col-form-label">Facebook Profile</label>
-                    <div class="col-md-8 col-lg-9">
-                      <input name="facebook" type="text" class="form-control" id="Facebook" value="https://facebook.com/#">
-                    </div>
-                  </div>
-
-                  <div class="row mb-3">
-                    <label for="Instagram" class="col-md-4 col-lg-3 col-form-label">Instagram Profile</label>
-                    <div class="col-md-8 col-lg-9">
-                      <input name="instagram" type="text" class="form-control" id="Instagram" value="https://instagram.com/#">
-                    </div>
-                  </div>
-
-                  <div class="row mb-3">
-                    <label for="Linkedin" class="col-md-4 col-lg-3 col-form-label">Linkedin Profile</label>
-                    <div class="col-md-8 col-lg-9">
-                      <input name="linkedin" type="text" class="form-control" id="Linkedin" value="https://linkedin.com/#">
-                    </div>
-                  </div>
+                  
 
                   <div class="text-center">
-                    <button type="submit" class="btn btn-primary">Save Changes</button>
+                    <button type="submit" class="btn btn-primary">ផ្លាស់ប្តូរ</button>
                   </div>
                 </form><!-- End Profile Edit Form -->
 
@@ -201,28 +175,28 @@
                 <form>
 
                   <div class="row mb-3">
-                    <label for="currentPassword" class="col-md-4 col-lg-3 col-form-label">Current Password</label>
+                    <label for="currentPassword" class="col-md-4 col-lg-3 col-form-label">Password បច្ចុប្បន្ន</label>
                     <div class="col-md-8 col-lg-9">
                       <input name="password" type="password" class="form-control" id="currentPassword">
                     </div>
                   </div>
 
                   <div class="row mb-3">
-                    <label for="newPassword" class="col-md-4 col-lg-3 col-form-label">New Password</label>
+                    <label for="newPassword" class="col-md-4 col-lg-3 col-form-label">Password ថ្មី</label>
                     <div class="col-md-8 col-lg-9">
                       <input name="newpassword" type="password" class="form-control" id="newPassword">
                     </div>
                   </div>
 
                   <div class="row mb-3">
-                    <label for="renewPassword" class="col-md-4 col-lg-3 col-form-label">Re-enter New Password</label>
+                    <label for="renewPassword" class="col-md-4 col-lg-3 col-form-label">បញ្ជាក់ Password ថ្មី</label>
                     <div class="col-md-8 col-lg-9">
                       <input name="renewpassword" type="password" class="form-control" id="renewPassword">
                     </div>
                   </div>
 
                   <div class="text-center">
-                    <button type="submit" class="btn btn-primary">Change Password</button>
+                    <button type="submit" class="btn btn-primary">ផ្លាស់ប្តូរ</button>
                   </div>
                 </form><!-- End Change Password Form -->
 
