396c7f29b16f297551747d742fff32bc96a513c0
phumentceo <roungchanrith8@gmail.com>
2024-12-30 13:38:50 +0700

    Setup and Config Project

--- database/migrations/0001_01_01_000000_create_admins_table.php
+++ database/migrations/0001_01_01_000000_create_admins_table.php
@@ -11,12 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('f_name');
+            $table->string('l_name');
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
@@ -42,7 +45,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admins');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
