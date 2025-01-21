84e049d6009746f9b1eb3f1ad68dc04cb9de3d4a
phumentceo <roungchanrith8@gmail.com>
2025-01-21 13:02:10 +0700

    sgfdsgfre

--- app/Mail/sendForgotPassword.php
+++ app/Mail/sendForgotPassword.php
@@ -1,7 +1,11 @@
 <?php
+
 namespace App\Mail;
+
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
