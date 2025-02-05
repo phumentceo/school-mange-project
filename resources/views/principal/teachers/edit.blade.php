da371520480ccb4a9437ea3af4ee33a746a814d1
phumentceo <roungchanrith8@gmail.com>
2025-02-05 13:21:01 +0700

    Teacher Update

--- resources/views/principal/teachers/edit.blade.php
+++ resources/views/principal/teachers/edit.blade.php
@@ -11,6 +11,7 @@
 
                 <div class="col-md-6">
                     <label class="form-label">ឈ្មោះជាភាសាខ្មែរ</label>
+                    <input type="hidden" name="created_by" value="{{ Auth::guard('admin')->user()->id }}">
                     <input type="text" class="form-control shadow-none @error('full_name') is-invalid @enderror"
                         name="full_name" value="{{ old('full_name', $teacher->full_name) }}" placeholder="ផាន សុផាត់">
                     @error('full_name')
@@ -27,18 +28,27 @@
                     @enderror
                 </div>
 
-                <div class="col-md-12">
+                <div class="col-md-6">
                     <label class="form-label"><strong>ភេទ</strong></label>
                     <select name="gender" class="form-control shadow-none @error('gender') is-invalid @enderror">
                         <option value="">ជ្រើសរើសភេទ</option>
-                        <option value="1" {{ old('gender', $teacher->gender) == '1' ? 'selected' : '' }}>ភេទប្រុស
-                        </option>
-                        <option value="2" {{ old('gender', $teacher->gender) == '2' ? 'selected' : '' }}>ភេទស្រី
-                        </option>
+                        <option value="1" {{ old('gender', $teacher->gender) == '1' ? 'selected' : '' }}>ភេទប្រុស</option>
+                        <option value="2" {{ old('gender', $teacher->gender) == '2' ? 'selected' : '' }}>ភេទស្រី</option>
                     </select>
                     @error('gender')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
+                </div>
+
+                <div class="col-md-6">
+                  <label class="form-label">លេខអត្តសញ្ញាណប័ណ្ណ</label>
+                  <input type="text" 
+                         class="form-control shadow-none @error('national_id') is-invalid @enderror" 
+                         name="national_id" 
+                         value="{{ old('national_id',$teacher->national_id) }}">
+                  @error('national_id')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                  @enderror
                 </div>
 
                 <div class="col-md-6">
@@ -52,13 +62,9 @@
 
                 <div class="col-md-6">
                     <label class="form-label">ស្ថានភាពជិវិត</label>
-                    <select name="marital_status"
-                        class="form-control shadow-none @error('marital_status') is-invalid @enderror">
-                        <option value="1"
-                            {{ old('marital_status', $teacher->marital_status) == '1' ? 'selected' : '' }}>មានគ្រួសារ
-                        </option>
-                        <option value="2"
-                            {{ old('marital_status', $teacher->marital_status) == '2' ? 'selected' : '' }}>នៅលីវ</option>
+                    <select name="marital_status" class="form-control shadow-none @error('marital_status') is-invalid @enderror">
+                        <option value="1" {{ old('marital_status', $teacher->marital_status) == '1' ? 'selected' : '' }}>មានគ្រួសារ</option>
+                        <option value="2" {{ old('marital_status', $teacher->marital_status) == '2' ? 'selected' : '' }}>នៅលីវ</option>
                     </select>
                     @error('marital_status')
                         <div class="invalid-feedback">{{ $message }}</div>
@@ -70,8 +76,7 @@
                     <select name="subject_id[]" id="subjects" class="form-control shadow-none @error('subject_id') is-invalid @enderror" multiple="multiple">
                         <option value="">ជ្រើសរើសមុខវិជ្ជា</option>
                         @foreach ($subjects as $subject)
-                            <option value="{{ $subject->id }}"
-                                {{ old('subject_id', $subject->teacher_id) == $teacher->id ? 'selected' : '' }}>
+                            <option value="{{ $subject->id }}" {{ in_array($subject->id, $subjectIds) ? 'selected' : '' }}>
                                 {{ $subject->subject_name }}
                             </option>
                         @endforeach
@@ -84,33 +89,37 @@
                 <div class="col-6">
                     <label class="form-label">មុខវិជ្ជាជំនាញ</label>
                     <input type="text" class="form-control shadow-none @error('specialization') is-invalid @enderror"
-                        name="specialization" value="{{ old('specialization', $teacher->specialization) }}"
-                        placeholder="ឯកទេស">
+                        name="specialization" value="{{ old('specialization', $teacher->specialization) }}" placeholder="ឯកទេស">
                     @error('specialization')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
 
                 <div class="col-6">
-                    <label class="form-label">កំរិតសញ្ញាប័ត្រ</label>
-                    <input type="text" class="form-control shadow-none @error('degree') is-invalid @enderror"
-                        name="degree" value="{{ old('degree', $teacher->degree) }}" placeholder="បរិញ្ញាប័ត្រ">
-                    @error('degree')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
+                  <label class="form-label">កំរិតវប្បធម៌</label>
+                  <input type="text" 
+                         class="form-control shadow-none @error('degree') is-invalid @enderror" 
+                         name="degree" 
+                         value="{{ old('degree',$teacher->degree) }}" 
+                         placeholder="បរិញ្ញាប័ត្រ">
+                  @error('degree')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                  @enderror
+              </div>
+      
+              <div class="col-6">
+                  <label class="form-label">បញ្ចប់ការសិក្សារមកពីសាលា</label>
+                  <input type="text" 
+                         class="form-control shadow-none @error('university') is-invalid @enderror" 
+                         name="university" 
+                         value="{{ old('university',$teacher->university) }}">
+                  @error('university')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                  @enderror
+              </div>
 
                 <div class="col-6">
-                    <label class="form-label">បញ្ចប់ការសិក្សារមកពីសាលា</label>
-                    <input type="text" class="form-control shadow-none @error('university') is-invalid @enderror"
-                        name="university" value="{{ old('university', $teacher->university) }}">
-                    @error('university')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-
-                <div class="col-6">
-                    <label class="form-label">លេខទូរស័ព្ទទំនាក់ទំនង</label>
+                    <label class="form-label">លេខទូរស័ព្ទ</label>
                     <input type="text" class="form-control shadow-none @error('phone') is-invalid @enderror"
                         name="phone" value="{{ old('phone', $teacher->phone) }}" placeholder="លេខទូរស័ព្ទ">
                     @error('phone')
@@ -128,7 +137,7 @@
                 </div>
 
                 <div class="col-md-3">
-                    <label class="form-label">ភូមិ​​​</label>
+                    <label class="form-label">ភូមិ</label>
                     <input type="text" class="form-control shadow-none" name="village"
                         value="{{ old('village', $address->village ?? '') }}">
                 </div>
@@ -150,12 +159,22 @@
                         value="{{ old('province', $address->province ?? '') }}">
                 </div>
 
-                <div class="col-12">
-                    <label class="form-label">ផ្សេងៗ</label>
-                    <textarea name="note" class="form-control shadow-none @error('note') is-invalid @enderror" rows="5">{{ old('note', $teacher->note) }}</textarea>
-                    @error('note')
-                        <div class="invalid-feedback">{{ $message }}</div>
+                <div class="col-md-12">
+                    <label class="form-label">ថ្ងៃចាប់ផ្តើមការងារ</label>
+                    <input type="text" 
+                           class="form-control shadow-none @error('hire_date') is-invalid @enderror" 
+                           name="hire_date" 
+                           value="{{ old('hire_date',$teacher->hire_date) }}" 
+                           placeholder="12 មិនា 2024">
+                    @error('hire_date')
+                      <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
+                </div>
+
+
+                <div class="col-12">
+                  <label class="form-label">ផ្សេងៗ</label>
+                  <textarea name="note" class="form-control shadow-none @error('note') is-invalid @enderror" rows="5">{{ old('note',$teacher->note) }}</textarea>
                 </div>
 
                 <div class="text-center">
@@ -170,18 +189,22 @@
         </div>
     </div>
 @endsection
+
 @section('scripts')
     <script>
+        
         $(document).ready(function() {
             $('#subjects').select2({
                 placeholder: 'ជ្រើសរើសមុខវិជ្ជា',
-                allowClear: true,
-                tags: false,
-                width: '100%'
+                allowClear: true, 
+                width: '100%',
+                minimumInputLength: 0,
+                tags: true 
             });
         });
 
-        document.getElementById('teacherForm').addEventListener('submit', function(event) {
+
+        document.getElementById('teacherForm').addEventListener('submit', function() {
             document.getElementById('spinner').style.display = 'inline-block';
             document.getElementById('submitBtn').disabled = true;
         });
