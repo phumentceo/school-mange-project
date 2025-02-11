b9f3e57d1b189f2857e4d671a429f8a9ceaed028
phumentceo <roungchanrith8@gmail.com>
2025-02-11 13:20:06 +0700

    Model Customize

--- app/Models/StudyClass.php
+++ app/Models/StudyClass.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class StudyClass extends Model
 {
-    //
+
+
+    use HasFactory;
+
+    protected $table ='study_classes';
+
+    protected $fillable = [
+        'name',
+        'class_level_id',
+        'homeroom_teacher',
+        'desk',
+        'fan',
+        'whiteboard',
+        'light',
+        'status',
+        'note'
+    ];
+
+    
+
+    public function teachers()
+    {
+        return $this->belongsToMany(Teacher::class, 'teacher_classes');
+    }
+
 }
