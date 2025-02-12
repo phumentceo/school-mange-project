7d12b66419455174f6e958c7cbdda19627ad2edc
phumentceo <roungchanrith8@gmail.com>
2025-02-12 15:17:45 +0700

    Seeder Data

--- database/seeders/StudyClassSeeder.php
+++ database/seeders/StudyClassSeeder.php
@@ -1,23 +1,41 @@
 <?php
 
 namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class StudyClassSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+   
     public function run(): void
     {
-        $classes = [
-            [
-                "name" => "7A",
-                "class_level_id" => 1,
-                "teacher_id" => 1,
-            ]
-        ];
+        $faker = Faker::create();
+
+        $levels = DB::table('student_levels')->get();
+        $teachers = DB::table('teachers')->pluck('id')->toArray();
+        $sections = ['A', 'B', 'C', 'D'];
+
+        $classes = [];
+
+        foreach ($levels as $level) {
+            foreach ($sections as $section) {
+                $classes[] = [
+                    'name' => $level->name . $section,
+                    'class_level_id' => $level->id,
+                    'homeroom_teacher' => $faker->randomElement($teachers),
+                    'desk' => 10,
+                    'fan' => $faker->randomElement([1, 2, 3]),
+                    'whiteboard' => 1,
+                    'status' => 1,
+                    'note' => null,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+            }
+        }
+
+        DB::table('study_classes')->insert($classes);
     }
+    
 }
