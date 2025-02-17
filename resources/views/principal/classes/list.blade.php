52c42058b074c914a49c2750998508e6432e462e
phumentceo <roungchanrith8@gmail.com>
2025-02-17 14:51:12 +0700

    Class

--- resources/views/principal/classes/list.blade.php
+++ resources/views/principal/classes/list.blade.php
@@ -9,7 +9,7 @@
                 </div>
             @endif
 
-            <h5 class="mb-4">មុខវិជ្ជាបង្រៀនទាំងអស់</h5>
+            <h5 class="mb-4">បន្ទាប់រៀនតាមថ្នាក់នីមួយៗ</h5>
             <table class="table table-hover datatable">
                 <thead>
                     <tr style="background-color: black; color: white;">
@@ -31,10 +31,25 @@
                                     <span class="text-danger">មិនមានគ្រូបន្ទុក</span>
                                 @endif
                             </td>
-                            <td>
+                            <td class=" d-flex align-items-center">
                                 <a class=" btn btn-success btn-sm" href="{{ route('admin.class.edit',$class->id) }}"><i class="bi bi-eye"></i></a>
                                 <a class=" btn btn-danger btn-sm" onclick="return confirm('Do you want to delete this?')" href="{{ route('admin.class.destroy',$class->id) }}" class=" text-danger"><i class="bi bi-trash2-fill"></i></a>
                                 <a class=" btn btn-primary btn-sm" href="{{ route('admin.class.edit',$class->id) }}"><i class="bi bi-pen"></i></a>
+                    
+                                <div class="dropdown">
+                                    <button class="btn btn-secondary btn-sm" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <i class="bi bi-calendar-day"></i>
+                                    </button>
+                                    
+                                    <ul class="dropdown-menu">
+                                      <li><a class="dropdown-item" href="#">ពិនិត្យមើលកាវិភាគ</a></li>
+                                      <li><a class="dropdown-item" href="#">បន្ថែមការវិភាគ</a></li>
+                                      <li><a class="dropdown-item" href="#">កែសម្រួលកាលវិភាគ</a></li>
+                                    </ul>
+                                </div>
+
+
+                                {{-- <i class="bi bi-calendar-day"></i> --}}
                                 
                             </td>
                         </tr>
@@ -43,6 +58,31 @@
             </table>
         </div>
     </div>
+
+
+    <!-- Button trigger modal -->
+  
+  
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
 @endsection
 
 @section('scripts')
